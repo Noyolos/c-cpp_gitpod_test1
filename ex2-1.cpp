1ef3b99a3f1ca71d4f3aff71c6925e1ac6f043b3
Noyolos <104434080+Noyolos@users.noreply.github.com>
2022-05-10 19:36:36 +0800

    Update ex2-1.cpp

--- ex2-1.cpp
+++ ex2-1.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int x;
-    int y;
-    std::cin >> x>>y;
-    std::cout << x*x+y*y << std::endl;
-
-
-    return 0;
+int main(void) {
+	int i;
+	float f;
+	char s[30];
+	char s1 = 'c';
+	
+	printf("Please input an integer \n");
+	scanf("%d", &i);
+	printf("Your input integer = %d\n", i);
+	
+	printf("Please input a float number \n");
+	scanf("%f", &f);
+	printf("Your input float number = %f\n", f);
+	
+	printf("Please input a string \n");
+	scanf("%s", s);
+	printf("Your input string = %s\n", s);
+	
+	printf("char display = %c\n", s1);
+	printf("char display = %x\n", s1);
+	
+	printf("Your input integer = %d\n", i + 2);
+	printf("Your input integer = %d\n", i - 2);
+	printf("Your input integer = %d\n", i * 2);
+	printf("Your input integer = %d\n", i / 2);
+	
+	return 0;
 }
