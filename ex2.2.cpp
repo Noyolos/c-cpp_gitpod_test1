bfe080bd4286943e9e6a67c8636ee09863cecb7f
Noyolos <104434080+Noyolos@users.noreply.github.com>
2022-05-10 19:06:47 +0800

    Update ex2.2.cpp

--- ex2.2.cpp
+++ ex2.2.cpp
@@ -1,1 +1,12 @@
 
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+	int r;
+	float pi=3.14;
+cin>>r;
+cout<<pow(r,2)*pi<<"为圆面积"<<endl;
+cout<<pi*(2*r)<<"为圆周长"<<endl;
+}	
